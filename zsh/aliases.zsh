7d976160da09bf93956b9e889d48b6d6ec984ea8
davisshaver <davisshaver@gmail.com>
2017-01-09 08:32:09 -0500

    Personal updates

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,3 +1,10 @@
 alias reload!='. ~/.zshrc'
 
 alias cls='clear' # Good 'ol Clear Screen command
+
+alias status='git status'
+alias push='git push origin'
+alias cm='git commit -m'
+alias pull='git pull origin'
+alias dd='~/broadway/www/digiday-redesign/wp-content/themes/digiday/'
+alias ton='~/broadway/www/techofnews/wp-content/themes/techofnews/'
