46e0ee4ef7b088e077fb155687100c5788655e5b
davisshaver <davisshaver@gmail.com>
2017-04-19 07:49:31 -0400

    Some changes to dotfiles

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,7 @@
 alias reload!='. ~/.zshrc'
-
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
 alias cls='clear' # Good 'ol Clear Screen command
-
 alias status='git status'
 alias push='git push origin'
 alias cm='git commit -m'
@@ -9,6 +9,7 @@ alias pull='git pull origin'
 alias add='git add .'
 alias co='git checkout'
 alias reset='git reset --hard head'
+alias conflicts='git diff --name-only --diff-filter=U'
 
 alias dd='~/broadway/www/digiday-redesign/wp-content/themes/digiday/'
 alias ton='~/broadway/www/techofnews/wp-content/themes/techofnews/'
